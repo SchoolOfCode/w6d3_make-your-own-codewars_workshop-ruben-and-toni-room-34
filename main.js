5a10d09c58abf00040c1ad2f22ffc7514569c4ba
Tonisaraiva <93171327+Tonisaraiva@users.noreply.github.com>
2021-12-23 14:32:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -37,31 +37,4 @@ const distributionList = [
 
 
 //👉 Write the function your CodeWarriors will start with below here:
-export function efficientRoadmap(distributionList){
-
-      // declare result array
-      let result = [];
-      // order the given array by present weight
-      distributionList.sort((a, b) => {
-          return b.weightOfPresent - a.weightOfPresent
-      });
-      // save heaviest object of the array, it will be the first element of the array result
-      const firstObject = distributionList[0];
-      // order the given array by alphabetically
-      distributionList.sort((a, b) => {
-          return a.city < b.city ? -1 : a.city > b.city ? 1 : 0;
-      });
-      // find the index of the heaviest object after been oreder alphabetically
-      const firstObjectIndex = distributionList.findIndex( obj => obj === firstObject );
-      // copy the the part of the array from the heaviest present to the last item to result
-      for(let i = firstObjectIndex; i < distributionList.length; i++) {
-          result.push(distributionList[i]);
-      }
-      // copy below the other objects in the array the remiand part of distributionList
-      for(let i = 0; i < firstObjectIndex ; i++) {
-          result.push(distributionList[i]);
-      }
-      // return the result array 
-      return result;
-  
-   }
+export function efficientRoadmap(distributionList){ }
