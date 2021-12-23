b1372a3d1f7a7919a5a0c1e26e7a036acb48d62e
Tonisaraiva <scramtoni@gmail.com>
2021-12-23 05:28:10 +0000

    kata with tests for check and update

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 /* 
 👉 Write your kata here!
 */
-// The Roadmap for Father Christmas
+// The Roadmap for Father Christmas' deliveries
 
  
-
-// Organise the destinations so that Father Christmas delivers the heaviest present first and spares his rein deers
+// Organise the destinations so that Father Christmas delivers the heaviest present first and spares his reindeers
 // a back breaking night of carrying heavy loads all over the country...
 // The second heaviest should be delivered second if it is in the same city as the heaviest, 
 // otherwise deliver all presents in that city and 
 // then move on to the next closest city.
 // Cities are organised by alphabetical order "a" is near "b", "b" is near "c", "c" is near "d"... 
-// Consider that "d" is nearer to "f" than it is to "a".
+// Consider that "d" is nearer to "f" than it is to "a" and that he goes down the alphabet but not up the alphabet.
  
 
 
-distributionList = [ 
+const distributionList = [ 
     { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"},
     { childname: "Toni", present: "Nutcracker" , weightOfPresent: 20, city: "London"},
     { childname: "Jenny", present: "Orange" , weightOfPresent: 100, city: "Birmingham"},
@@ -31,4 +30,6 @@ distributionList = [
 
 
 //👉 Write the function your CodeWarriors will start with below here:
-function efficientRoadmap (distributionList){ }
+export function efficientRoadMap (distributionList){ }
+
+
--- main.test.js
+++ main.test.js
@@ -1,118 +1,113 @@
+import {efficientRoadMap} from "./main.js"
 
-const distributionList =[ 
-    { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"},
-    { childname: "Toni", present: "Nutcracker" , weightOfPresent: 20, city: "London"},
-    { childname: "Jenny", present: "Orange" , weightOfPresent: 100, city: "Birmingham"},
-    { childname: "Qing", present: "Bicycle" , weightOfPresent: 5000, city: "Liverpool"},
-    { childname: "Maria", present: "Motorbike" , weightOfPresent: 25000, city: "Southampton"},
-    { childname: "David", present: "TV" , weightOfPresent: 3000, city: "Southampton"},
-    { childname: "Jasien", present: "Phone" , weightOfPresent: 300, city: "London"},
-    { childname: "Joseph", present: "Doll" , weightOfPresent: 250, city: "Birmingham"},
-    { childname: "Chris", present: "Art set" , weightOfPresent: 300, city: "Birmingham"},
-    { childname: "Liz", present: "Glasses" , weightOfPresent:200, city: "Liverpool"},
-    { childname: "Florence", present: "Book" , weightOfPresent: 1000, city: "London"},
- ]
+// before releasing: need to change all the actual to const actual = efficientRoadMap();
 
-
-
-
-
-//👉 Write your tests below here:
-describe(`organised array`, () =>{
-/*It(`see that weight is considered`, () =>{
-//Arrange
-const expected = [
-    { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"},
-    { childname: "Maria", present: "Motorbike" , weightOfPresent: 25000, city: "Southampton"},
-    { childname: "Qing", present: "Bicycle" , weightOfPresent: 5000, city: "Liverpool"},
-    { childname: "David", present: "TV" , weightOfPresent: 3000, city: "Southampton"},
-    { childname: "Florence", present: "Book" , weightOfPresent: 1000, city: "London"},
-    { childname: "Chris", present: "Art set" , weightOfPresent: 300, city: "Birmingham"},
-    { childname: "Jasien", present: "Phone" , weightOfPresent: 300, city: "London"},
-    { childname: "Joseph", present: "Doll" , weightOfPresent: 250, city: "Birmingham"},
-    { childname: "Liz", present: "Glasses" , weightOfPresent:200, city: "Liverpool"},
-    { childname: "Jenny", present: "Orange" , weightOfPresent: 100, city: "Birmingham"},
-    { childname: "Toni", present: "Nutcracker" , weightOfPresent: 20, city: "London"},
- ];
-
-//Act   
-const actual = efficientRoadmap();
-
-//Assert
-expect(actual).toStrictlyEqual(expected)
-
-
+describe(`organised distribution list as expected for efficient distribution`, () =>{
+    it(`The first present in the distribution list is the heaviest`, () => {
+    // Arrange 
+    const expected =  expectedDistributionList[0];   
+    // Act  
+    const actual = { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"};
+    // Assert 
+    expect(actual).toEqual(expected);
+});
+    it(`The second ciy in the distribution list is Southampton following London`, () => {
+    // Arrange 
+    const expected = expectedDistributionList[4];
+    // Act  
+    const actual = { childname: "Maria", present: "Motorbike" , weightOfPresent: 25000, city: "Southampton"};  
+    // Assert 
+    expect(actual).toEqual(expected);
 });
 
-It(`see that city order is considered`, ()=>{
-//Arrange
-const expected = [
-    { childname: "Chris", present: "Art set" , weightOfPresent: 300, city: "Birmingham"},
-    { childname: "Joseph", present: "Doll" , weightOfPresent: 250, city: "Birmingham"},
-    { childname: "Jenny", present: "Orange" , weightOfPresent: 100, city: "Birmingham"},
-    { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"},
-    { childname: "Florence", present: "Book" , weightOfPresent: 1000, city: "London"},
-    { childname: "Jasien", present: "Phone" , weightOfPresent: 300, city: "London"},
-    { childname: "Toni", present: "Nutcracker" , weightOfPresent: 20, city: "London"},
-    { childname: "Qing", present: "Bicycle" , weightOfPresent: 5000, city: "Liverpool"},
-    { childname: "Liz", present: "Glasses" , weightOfPresent:200, city: "Liverpool"},
-    { childname: "David", present: "TV" , weightOfPresent: 3000, city: "Southampton"},
-    { childname: "Maria", present: "Motorbike" , weightOfPresent: 25000, city: "Southampton"},
-];
-//Act   
-const actual = efficientRoadmap();
-
-//Assert
-expect(actual).toStrictlyEqual(expected)
-
+    it(`The third city in the distribution list is Birmingham following Southampton`, () => {
+    // Arrange 
+    const expected = expectedDistributionList[6];
+    // Act  
+    const actual = { childname: "Chris", present: "Art set" , weightOfPresent: 300, city: "Birmingham"};
+    // Assert 
+    expect(actual).toEqual(expected);
 });
-*/
-it(`check whether the first object is the heaviest in the distribution list`, ()=>{
-    //Arrange
-    const expected = distributionList.sort((a,b) => {
-        return (b.weightOfPresent - a.weightOfPresent) || (a.city < b.city) ? -1 : (a.city > b.city) ? 1 : 0; });
-    console.log(expected); 
 
-
-    // Act
-    const actual = [
-    { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"},
-    { childname: "Florence", present: "Book" , weightOfPresent: 1000, city: "London"},
-    { childname: "Jasien", present: "Phone" , weightOfPresent: 300, city: "London"},
-    { childname: "Toni", present: "Nutcracker" , weightOfPresent: 20, city: "London"},
-    { childname: "Maria", present: "Motorbike" , weightOfPresent: 25000, city: "Southampton"},
-    { childname: "David", present: "TV" , weightOfPresent: 3000, city: "Southampton"},
-    { childname: "Chris", present: "Art set" , weightOfPresent: 300, city: "Birmingham"},
-    { childname: "Joseph", present: "Doll" , weightOfPresent: 250, city: "Birmingham"},
-    { childname: "Jenny", present: "Orange" , weightOfPresent: 100, city: "Birmingham"},
-    { childname: "Qing", present: "Bicycle" , weightOfPresent: 5000, city: "Liverpool"},
-    { childname: "Liz", present: "Glasses" , weightOfPresent:200, city: "Liverpool"},
- ];
-        
-    // Assert
-    expect(actual[0]).toEqual(expected[0]);
+    it(`The fourth city in the distribution list is Liverpool following Birmingham`, () => {
+    // Arrange 
+    const expected = expectedDistributionList[9];
+    // Act  
+    const actual = { childname: "Qing", present: "Bicycle" , weightOfPresent: 5000, city: "Liverpool"};     
+    // Assert 
+    expect(actual).toEqual(expected);
 });
-/*
-it(`check whether the first object is the heaviest in the distribution list`, ()=>{
-    //Arrange
-    const expected = distributionList[0]
+
+    it(`The second heaviest present on the list is in London and is the book`, () => {
+    // Arrange 
+    const expected = expectedDistributionList[1];
+    // Act  
+    const actual = { childname: "Florence", present: "Book" , weightOfPresent: 1000, city: "London"};
+    // Assert 
+    expect(actual).toEqual(expected);
+});
+
+    it(`The second heaviest present in Southampton is the TV`, () => {
+    // Arrange 
+    const expected = expectedDistributionList[5];
+    // Act  
+    const actual =  { childname: "David", present: "TV" , weightOfPresent: 3000, city: "Southampton"};
      
-    // Act
-    const actual = efficientRoadmap();
-        
-    // Assert
-    expect(actual[0]).toStrictlyEqual(expected)
+    // Assert 
+    expect(actual).toEqual(expected);
 });
 
+    it(`The second heaviest present in Birmingham is the doll`, () => {
+    // Arrange 
+    const expected = expectedDistributionList[7];
+    // Act  
+    const actual = { childname: "Joseph", present: "Doll" , weightOfPresent: 250, city: "Birmingham"};
+    // Assert 
+    expect(actual).toEqual(expected);
+});
+it(`see that both weight and cities are organised first by weight then by city `, ()=>{
+    //Arrange
+    
+    const expected = [
+        { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"},
+        { childname: "Florence", present: "Book" , weightOfPresent: 1000, city: "London"},
+        { childname: "Jasien", present: "Phone" , weightOfPresent: 300, city: "London"},
+        { childname: "Toni", present: "Nutcracker" , weightOfPresent: 20, city: "London"},
+        { childname: "Maria", present: "Motorbike" , weightOfPresent: 25000, city: "Southampton"},
+        { childname: "David", present: "TV" , weightOfPresent: 3000, city: "Southampton"},
+        { childname: "Chris", present: "Art set" , weightOfPresent: 300, city: "Birmingham"},
+        { childname: "Joseph", present: "Doll" , weightOfPresent: 250, city: "Birmingham"},
+        { childname: "Jenny", present: "Orange" , weightOfPresent: 100, city: "Birmingham"},
+        { childname: "Qing", present: "Bicycle" , weightOfPresent: 5000, city: "Liverpool"},
+        { childname: "Liz", present: "Glasses" , weightOfPresent:200, city: "Liverpool"},
+     ];
+    
+    // Act 
+    const actual = [
+        { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"},
+        { childname: "Florence", present: "Book" , weightOfPresent: 1000, city: "London"},
+        { childname: "Jasien", present: "Phone" , weightOfPresent: 300, city: "London"},
+        { childname: "Toni", present: "Nutcracker" , weightOfPresent: 20, city: "London"},
+        { childname: "Maria", present: "Motorbike" , weightOfPresent: 25000, city: "Southampton"},
+        { childname: "David", present: "TV" , weightOfPresent: 3000, city: "Southampton"},
+        { childname: "Chris", present: "Art set" , weightOfPresent: 300, city: "Birmingham"},
+        { childname: "Joseph", present: "Doll" , weightOfPresent: 250, city: "Birmingham"},
+        { childname: "Jenny", present: "Orange" , weightOfPresent: 100, city: "Birmingham"},
+        { childname: "Qing", present: "Bicycle" , weightOfPresent: 5000, city: "Liverpool"},
+        { childname: "Liz", present: "Glasses" , weightOfPresent:200, city: "Liverpool"},
+     ];
+    // Assert
+    expect(actual).toEqual(expected);
+      
+});
+
+})
 
 
 
 
 
-
-it(`see that both weight and cities are organised `, ()=>{
-//Arrange
-const expected = [
+const expectedDistributionList = [
     { childname: "Ruben", present: "Car" , weightOfPresent: 100000, city: "London"},
     { childname: "Florence", present: "Book" , weightOfPresent: 1000, city: "London"},
     { childname: "Jasien", present: "Phone" , weightOfPresent: 300, city: "London"},
@@ -125,12 +120,3 @@ const expected = [
     { childname: "Qing", present: "Bicycle" , weightOfPresent: 5000, city: "Liverpool"},
     { childname: "Liz", present: "Glasses" , weightOfPresent:200, city: "Liverpool"},
  ];
-//Act   
-const actual = efficientRoadmap();
-
-//Assert
-expect(actual).toStrictlyEqual(expected)
-
-})*/
-
-});
